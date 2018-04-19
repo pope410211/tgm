178c4160882b07433b3a3293d11d261d811a2a19
Nick Pope <pope410211@gmail.com>
2018-04-18 22:31:54 -0400

    tmp stopping point

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccount');
 
-console.log('service', serviceAccount);
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://turtlegirlsmarket-64f1c.firebaseio.com'
 });
 
-
-
+const apiKey = functions.config().tgmconfig;
+console.log('apiKey', apiKey);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
