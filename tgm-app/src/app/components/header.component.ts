80a5fd3ddbbe0293829464066355489ee7556d5d
Nick Pope <pope410211@gmail.com>
2018-04-21 15:01:30 -0400

    B2B - Pre Mobile Ready, pushing up as a front facing site - temp

--- tgm-app/src/app/components/header.component.ts
+++ tgm-app/src/app/components/header.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     template: `
-        <nav class="navbar navbar-expand-lg sticky-top" id="b2cNavBar">
+        <nav class="navbar navbar-expand-lg sticky-top open-sans" id="b2cNavBar">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarToggle" aria-controls="navBarToggle" aria-expanded="false" aria-label="Toggle Navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navBarToggle">
-                <span class="navbar-brand mb-01 h1">Turtle Girls Market</span>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">About</a>
+                <a class="navbar-brand" href="#">
+                    <img [src]="logoImagePath" width="30" height="30" alt="Turtle Girls Market Logo">
+                </a>
+                <span class="navbar-brand mb-01 h1 br-clr-wh">Turtle Girls Market</span>
+                <ul class="navbar-nav ml-auto b2cLinkList">
+                    <li class="nav-item b2cLinkItem">
+                        <a class="nav-link b2cLink" href="#tgm">The Turlte Girls</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#tgm">The Turlte Girls</a>
+                    <li class="nav-item b2cLinkItem">
+                        <a class="nav-link b2cLink" href="#about">About</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#gal">Gallery</a>
+                    <li class="nav-item b2cLinkItem">
+                        <a class="nav-link b2cLink" href="#gal">Gallery</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link">Vendor Login</a>
+                    <li class="nav-item b2cLinkItem">
+                        <a class="nav-link b2cLink">Vendor Login</a>
                     </li>
                 </ul>
             </div>
@@ -28,4 +31,13 @@ import { Component } from '@angular/core';
     `
 })
 
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit{
+    logoImagePath: string;
+
+    public constructor() {
+        this.logoImagePath = 'assets/logos/tgm_logo-WH.png'
+    }
+
+    ngOnInit() {}
+    
+}
