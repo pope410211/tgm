b9338ed660793f70ed6192332219afd6d9256adb
Nick Pope <pope410211@gmail.com>
2018-04-21 20:26:47 -0400

    Created a Login Modal:
    Added a Generic `_modal.scss` for use in Login Modal and Future modals.
    Add updated necessary components to ensure modals success.

--- tgm-app/src/app/app.module.ts
+++ tgm-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RoutesModule } from './routes.module';
+// Components
 import { AppComponent } from './app.component';
 import { MainComponent } from '../app/components/main.component';
 import { HeaderComponent } from '../app/components/header.component';
 import { LoginModalComponent } from '../app/components/login-modal.component';
+// Services
+import { AuthService } from '../app/services/auth-service.component';
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
     RoutesModule,
+    AngularFireModule,
+    AngularFireAuthModule
   ],
   exports: [
     RoutesModule,
@@ -25,7 +33,11 @@ import { LoginModalComponent } from '../app/components/login-modal.component';
     HeaderComponent,
     LoginModalComponent
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
